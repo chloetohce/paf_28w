50ee9e59d3740cad029cc1758b147f70cba633be
chloetohce <chloetohce@gmail.com>
2025-01-21 15:54:58 +0800

    Added: game model

--- src/main/java/paf/workshop/paf_26w/model/Game.java
+++ src/main/java/paf/workshop/paf_26w/model/Game.java
@@ -4,5 +4,31 @@ public class Game {
     private int gid;
 
     private String name;
+
+    public Game() {
+    }
+
+    public Game(int gid, String name) {
+        this.gid = gid;
+        this.name = name;
+    }
+
+    public int getGid() {
+        return gid;
+    }
+
+    public void setGid(int gid) {
+        this.gid = gid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    
     
 }
