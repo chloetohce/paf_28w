14c29e867774da8f937d5afb1e55ec96546987fb
chloetohce <chloetohce@gmail.com>
2025-01-21 15:50:41 +0800

    init

---
+++ src/main/java/paf/workshop/paf_26w/Paf26wApplication.java
@@ -0,0 +1,13 @@
+package paf.workshop.paf_26w;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Paf26wApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Paf26wApplication.class, args);
+	}
+
+}
---
+++ src/main/java/paf/workshop/paf_26w/model/Game.java
@@ -0,0 +1,8 @@
+package paf.workshop.paf_26w.model;
+
+public class Game {
+    private int gid;
+
+    private String name;
+    
+}
