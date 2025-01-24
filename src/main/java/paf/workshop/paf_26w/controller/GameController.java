ae1467c4775ad3d0fe3a3d3653ca612780cf9844
chloetohce <chloetohce@gmail.com>
2025-01-24 11:32:12 +0800

    Completed

--- src/main/java/paf/workshop/paf_26w/controller/GameController.java
+++ src/main/java/paf/workshop/paf_26w/controller/GameController.java
@@ -5,6 +5,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -27,5 +28,20 @@ public class GameController {
         JsonObject result = service.getGamesLimit(limit, offset);
         return new ResponseEntity<>(result.toString(), HttpStatus.OK);
     }
+
+    @GetMapping(path="/rank", produces=MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<String> getGamesByRank(@RequestParam(required=false, defaultValue="25") int limit,
+            @RequestParam(required=false, defaultValue = "0") long offset) {
+
+        JsonObject result = service.getGamesByRank(limit, offset);
+        return new ResponseEntity<>(result.toString(), HttpStatus.OK);
+    }
+
+    @GetMapping(path="/{id}", produces=MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<String> getGameById(@PathVariable String id) {
+        JsonObject result = service.getGameById(Integer.parseInt(id));
+        return new ResponseEntity<>(result.toString(), HttpStatus.OK);
+    }
+    
     
 }
--- src/main/java/paf/workshop/paf_26w/service/GameService.java
+++ src/main/java/paf/workshop/paf_26w/service/GameService.java
@@ -1,5 +1,6 @@
 package paf.workshop.paf_26w.service;
 
+import java.io.StringReader;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
@@ -9,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import jakarta.json.Json;
+import jakarta.json.JsonArray;
 import jakarta.json.JsonArrayBuilder;
 import jakarta.json.JsonObject;
 import jakarta.json.JsonObjectBuilder;
@@ -19,19 +21,52 @@ public class GameService {
 
     @Autowired
     private GameRepository repository;
-    
-    public JsonObject getGamesLimit(int limit, long offset) {
-        List<Document> result = repository.getGamesLimit(limit, offset);
-        JsonArrayBuilder gamesBuilder = Json.createArrayBuilder();
-        for (Document d : result) {
-            gamesBuilder.add(d.toJson());
-        }
+
+    private JsonObject buildResponseGames(JsonArray arr, int limit, long offset) {
         JsonObjectBuilder response = Json.createObjectBuilder();
-        response.add("games", gamesBuilder.build());
+        response.add("games", arr);
         response.add("offset", offset);
         response.add("limit", limit);
         response.add("total", repository.getCount());
         response.add("timestamp", LocalDateTime.now().format(DateTimeFormatter.ISO_LOCAL_DATE_TIME));
         return response.build();
+    }
+    
+    public JsonObject getGamesLimit(int limit, long offset) {
+        List<Document> result = repository.getGamesLimit(limit, offset);
+        JsonArrayBuilder gamesBuilder = Json.createArrayBuilder();
+        for (Document d : result) {
+            JsonObject game = Json.createObjectBuilder()
+                .add("game_id", d.getInteger("_id"))
+                .add("name", d.getString("name"))
+                .build();
+            gamesBuilder.add(game);
+        }
+        
+        return buildResponseGames(gamesBuilder.build(), limit, offset);
+    }
+
+    public JsonObject getGamesByRank(int limit, long offset) {
+        List<Document> result = repository.getGamesByRank(limit, offset);
+        JsonArrayBuilder gamesBuilder = Json.createArrayBuilder();
+        for (Document d : result) {
+            JsonObject game = Json.createObjectBuilder()
+                .add("game_id", d.getInteger("_id"))
+                .add("name", d.getString("name"))
+                .add("ranking", d.getInteger("ranking"))
+                .build();
+            gamesBuilder.add(game);
+        }
+        return buildResponseGames(gamesBuilder.build(), limit, offset);
+    }
+
+    public JsonObject getGameById(int id) {
+        Document result = repository.getGameById(id);
+        return Json.createObjectBuilder(
+            Json.createReader(new StringReader(result.toJson()
+                .replace("_id", "game_id")))
+            .readObject())
+            .add("timestamp", LocalDateTime.now().toString())
+            .build();
     }
 }
